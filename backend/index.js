ea7ddd6211f1bcb29ac8e2b287ede2700a373664
nguyenvandung99 <dung.nguyen>
2020-10-09 16:06:03 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require("./config/key");
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+mongoose.connect(config.mongoURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.use('/', require('./routes/user'));
+
+app.listen(port, () => {
+  console.log(`🌻  ${port} 🌻`)
+})
